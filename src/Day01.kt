be63f70f9f584ae629e28cf7b5414b96619cb3be
Muhammad Hakim Asya'ari <hakimrie@gmail.com>
2022-12-17 21:47:55 +0700

    Day 1 2022 part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,18 @@
 fun main() {
     var maxCalories = 0
     var totalCalories = 0
+    val eachElvesTotalCalories = mutableListOf<Int>()
     val testInput = readInput("Day01_test")
     testInput.forEach {
         if (it.isEmpty()) {
+            eachElvesTotalCalories.add(totalCalories)
             if (maxCalories < totalCalories) maxCalories = totalCalories
             totalCalories = 0
         } else {
             totalCalories += it.toInt()
         }
     }
-
-    println("$maxCalories") // 71023
+    println("part 1: $maxCalories") // 71023
+    eachElvesTotalCalories.sortDescending()
+    println("part 2: ${eachElvesTotalCalories.take(3).sum()}") // 206289
 }
